e55da7c362f278151268c598943decc664cd4068
xshom <shomurodow.sh@gmail.com>
2016-11-30 09:51:44 +0500

    change border-color of message

--- mod1_ang_solution/app.js
+++ mod1_ang_solution/app.js
@@ -19,16 +19,19 @@ function LCController($scope) {
     // console.log(counter);
     if (counter === 0) {
       $scope.message = 'Please enter data first';
+      $scope.emptiness = "true";
 
 
     }
     else if (counter < 4) {
       $scope.message = "Enjoy";
+      $scope.emptiness = "false";
 
 
     }
     else if (counter > 3) {
       $scope.message = "Too much";
+      $scope.emptiness = "false";
 
 
     }
